357603eea3e3cea2465c8d07bb442e7872d6d51f
Diego Naranjo <contact@diegonaranjo.co>
2023-11-08 19:04:32 -0500

    clase00

---
+++ clase00/index.js
@@ -0,0 +1,20 @@
+nombre = 'Mate' // string
+edad = 4 // number
+edad1 = `4` // string
+edad2 = '4' // string
+edad3 = '4' // string
+isStudent = false // boolean
+family = {
+  brother: 'Titín',
+  mom: 'Carito',
+} // object
+hobbies = ['play futbol', 'dance', 'listen to music'] // array
+
+console.log(nombre)
+console.log(edad1)
+console.log(edad2)
+console.log(edad3)
+console.log(isStudent)
+console.log(family)
+console.log(family)
+console.log(hobbies)
